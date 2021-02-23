0aee5fe2b79b97b958526c4a19529cf04d8041a2
zevdvlpr <zevdvlpr@gmail.com>
2021-02-23 12:50:15 -0300

    💡 chore: components

---
+++ src/components/Profile.tsx
@@ -0,0 +1,19 @@
+import styles from '../styles/components/Profile.module.css';
+
+export function Profile() {
+  return (
+    <div className={styles.profileContainer}>
+      <img src="https://github.com/zevdvlpr.png" alt="Zev" />
+
+      <div>
+        <strong>Zev</strong>
+
+        <p>
+          <img src="icons/level.svg" alt="Level" />
+          
+          Level 1
+        </p>
+      </div>
+    </div>
+  );
+}
