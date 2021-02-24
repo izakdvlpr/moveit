831cc682eda3d60141380df8cf258ad44bdb75ac
zevdvlpr <zevdvlpr@gmail.com>
2021-02-24 15:34:06 -0300

    🔨 refactor: add user xp information

--- src/components/ExperienceBar.tsx
+++ src/components/ExperienceBar.tsx
@@ -1,17 +1,32 @@
+import { useContext } from 'react';
+
+import { ChallengesContext } from '../contexts/ChallengesContext';
+
 import styles from '../styles/components/ExperienceBar.module.css';
 
 export function ExperienceBar() {
+  const { currentExperience, experienceToNextLevel } = useContext(
+    ChallengesContext,
+  );
+
+  const precentToNextLevel = Math.round(
+    (currentExperience * 100) / experienceToNextLevel,
+  );
+
   return (
     <header className={styles.experienceBar}>
       <span>0 xp</span>
       <div>
-        <div style={{ width: '50%' }} />
+        <div style={{ width: `${precentToNextLevel}%` }} />
 
-        <span className={styles.currentExperience} style={{ left: '50%' }}>
-          300 xp
+        <span
+          className={styles.currentExperience}
+          style={{ left: `${precentToNextLevel}%` }}
+        >
+          {currentExperience > 0 && `${currentExperience} xp`}
         </span>
       </div>
-      <span>600 xp</span>
+      <span>{experienceToNextLevel} xp</span>
     </header>
   );
 }
