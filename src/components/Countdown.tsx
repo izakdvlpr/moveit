76202fbc280a26e61f8f75439dfe4233d1fe5751
zevdvlpr <zevdvlpr@gmail.com>
2021-02-24 15:42:41 -0300

    💡 chore: add functionality to time and how it should behave

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -1,31 +1,64 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useContext } from 'react';
+import Head from 'next/head';
+import UseAnimations from 'react-useanimations';
+
+import radioButton from 'react-useanimations/lib/radioButton';
+import * as Icons from 'react-icons/io';
+
+import { ChallengesContext } from '../contexts/ChallengesContext';
 
 import styles from '../styles/components/Countdown.module.css';
 
+let countdownTimeout: NodeJS.Timeout;
+
 export function Countdown() {
-  const [time, setTime] = useState(25 * 60);
-  const [active, setActive] = useState(false);
+  const { startNewChallenge } = useContext(ChallengesContext);
+
+  const [time, setTime] = useState(0.1 * 60);
+  const [isActive, setIsActive] = useState(false);
+  const [hasFinished, setHasFinished] = useState(false);
 
   const minutes = Math.floor(time / 60);
-  const seconds = time % 60;
+  const seconds = time % 60;  
 
-  const [minuteLeft, minuteRight] = String(minutes).padStart(2, '0').split('');
-  const [secondLeft, secondRight] = String(seconds).padStart(2, '0').split('');
+  const minutesParsed = String(minutes).padStart(2, '0').split('');
+  const secondsParsed = String(seconds).padStart(2, '0').split('');
+
+  const [minuteLeft, minuteRight] = minutesParsed;
+  const [secondLeft, secondRight] = secondsParsed;
 
   function startCountdown() {
-    setActive(true);
+    setIsActive(true);
+  }
+
+  function resetCountdown() {
+    clearTimeout(countdownTimeout);
+    setIsActive(false);
+    setTime(0.1 * 60);
   }
 
   useEffect(() => {
-    if (active && time > 0) {
-      setTimeout(() => {
+    if (isActive && time > 0) {
+      countdownTimeout = setTimeout(() => {
         setTime(time - 1);
       }, 1000);
+    } else if (isActive && time === 0) {
+      setHasFinished(true);
+      setIsActive(false);
+      startNewChallenge();
     }
-  }, [active, time]);
+  }, [isActive, time]);
 
   return (
     <div>
+      <Head>
+        {isActive && (
+          <title>
+            ({minuteLeft + minuteRight}:{secondLeft + secondRight}) | Move.it
+          </title>
+        )}
+      </Head>
+
       <div className={styles.countdownContainer}>
         <div>
           <span>{minuteLeft}</span>
@@ -40,13 +73,37 @@ export function Countdown() {
         </div>
       </div>
 
-      <button
-        type="button"
-        className={styles.countdownButton}
-        onClick={startCountdown}
-      >
-        Iniciar um ciclo
-      </button>
+      {hasFinished ? (
+        <button disabled className={styles.countdownButton}>
+          Ciclo encerrado
+          <UseAnimations
+            animation={radioButton}
+            autoplay
+            size={36}
+            strokeColor="var(--green)"
+          />
+        </button>
+      ) : (
+        <>
+          {isActive ? (
+            <button
+              type="button"
+              className={`${styles.countdownButton} ${styles.countdownButtonActive}`}
+              onClick={resetCountdown}
+            >
+              Abandonar ciclo <Icons.IoMdClose size={25} />
+            </button>
+          ) : (
+            <button
+              type="button"
+              className={styles.countdownButton}
+              onClick={startCountdown}
+            >
+              Iniciar um ciclo <Icons.IoMdPlay size={16} />
+            </button>
+          )}
+        </>
+      )}
     </div>
   );
 }
