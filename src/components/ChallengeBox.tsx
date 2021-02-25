b71f73541b1f6c7a3dc0b5eb18d8b7aaa53aac9e
zevdvlpr <zevdvlpr@gmail.com>
2021-02-25 14:11:17 -0300

    ⚙️ perf: import countdown context

--- src/components/ChallengeBox.tsx
+++ src/components/ChallengeBox.tsx
@@ -1,11 +1,25 @@
 import { useContext } from 'react';
 
 import { ChallengesContext } from '../contexts/ChallengesContext';
+import { CountdownContext } from '../contexts/CountdownContext';
 
 import styles from '../styles/components/ChallengeBox.module.css';
 
 export function ChallengeBox() {
-  const { activeChallenge, resetChallenge } = useContext(ChallengesContext);
+  const { activeChallenge, resetChallenge, completeChallenge } = useContext(
+    ChallengesContext,
+  );
+  const { resetCountdown } = useContext(CountdownContext);
+
+  function handleChallengeSucceeded() {
+    completeChallenge();
+    resetCountdown();
+  }
+
+  function handleChallengeFailed() {
+    resetChallenge();
+    resetCountdown();
+  }
 
   return (
     <div className={styles.challengeBoxContainer}>
@@ -28,12 +42,16 @@ export function ChallengeBox() {
             <button
               type="button"
               className={styles.challengeFailedButton}
-              onClick={resetChallenge}
+              onClick={handleChallengeFailed}
             >
               Falhei
             </button>
 
-            <button type="button" className={styles.challengeSucceedButton}>
+            <button
+              type="button"
+              className={styles.challengeSucceedButton}
+              onClick={handleChallengeSucceeded}
+            >
               Completei
             </button>
           </footer>
