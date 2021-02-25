d8132a10c6253f36d658785e6e595b2940ce4329
zevdvlpr <zevdvlpr@gmail.com>
2021-02-25 14:11:31 -0300

    ⚙️ perf: import countdown context

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -1,53 +1,26 @@
-import { useState, useEffect, useContext } from 'react';
+import { useContext } from 'react';
 import Head from 'next/head';
 import UseAnimations from 'react-useanimations';
 
 import radioButton from 'react-useanimations/lib/radioButton';
 import * as Icons from 'react-icons/io';
 
-import { ChallengesContext } from '../contexts/ChallengesContext';
+import { CountdownContext } from '../contexts/CountdownContext';
 
 import styles from '../styles/components/Countdown.module.css';
 
-let countdownTimeout: NodeJS.Timeout;
-
 export function Countdown() {
-  const { startNewChallenge } = useContext(ChallengesContext);
+  const {
+    hasFinished,
+    isActive,
+    minutes,
+    seconds,
+    resetCountdown,
+    startCountdown,
+  } = useContext(CountdownContext);
 
-  const [time, setTime] = useState(0.1 * 60);
-  const [isActive, setIsActive] = useState(false);
-  const [hasFinished, setHasFinished] = useState(false);
-
-  const minutes = Math.floor(time / 60);
-  const seconds = time % 60;  
-
-  const minutesParsed = String(minutes).padStart(2, '0').split('');
-  const secondsParsed = String(seconds).padStart(2, '0').split('');
-
-  const [minuteLeft, minuteRight] = minutesParsed;
-  const [secondLeft, secondRight] = secondsParsed;
-
-  function startCountdown() {
-    setIsActive(true);
-  }
-
-  function resetCountdown() {
-    clearTimeout(countdownTimeout);
-    setIsActive(false);
-    setTime(0.1 * 60);
-  }
-
-  useEffect(() => {
-    if (isActive && time > 0) {
-      countdownTimeout = setTimeout(() => {
-        setTime(time - 1);
-      }, 1000);
-    } else if (isActive && time === 0) {
-      setHasFinished(true);
-      setIsActive(false);
-      startNewChallenge();
-    }
-  }, [isActive, time]);
+  const [minuteLeft, minuteRight] = String(minutes).padStart(2, '0').split('');
+  const [secondLeft, secondRight] = String(seconds).padStart(2, '0').split('');
 
   return (
     <div>
