345787d0138eac5fae73d97d02ebe55beb2bcf8c
zevdvlpr <zevdvlpr@gmail.com>
2021-02-25 14:10:49 -0300

    🔨 refactor: add typing to the app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,10 @@
+import { AppProps } from 'next/app';
+
 import { ChallengesProvider } from '../contexts/ChallengesContext';
 
 import '../styles/global.css';
 
-export default function App({ Component, pageProps }) {  
+export default function App({ Component, pageProps }: AppProps) {
   return (
     <ChallengesProvider>
       <Component {...pageProps} />;
