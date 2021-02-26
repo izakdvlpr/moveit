813f2da05beae081ddbd5d8dbdeb617a3babcc93
zevdvlpr <zevdvlpr@gmail.com>
2021-02-26 14:57:31 -0300

    💡 chore: login page

---
+++ src/pages/login.tsx
@@ -0,0 +1,27 @@
+import Head from 'next/head';
+
+import styles from '../styles/pages/Login.module.css';
+
+import { LoginForm } from '../components/LoginForm';
+
+export default function Login() {
+  return (
+    <div className={styles.loginBody}>
+      <div className={styles.loginContainer}>
+        <Head>
+          <title>Login | Move.it</title>
+        </Head>
+
+        <section>
+          <div className={styles.loginSymbol}>
+            <img src="/logo-linear.svg" alt="Symbol" />
+          </div>
+
+          <div>
+            <LoginForm />
+          </div>
+        </section>
+      </div>
+    </div>
+  );
+}
