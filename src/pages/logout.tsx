f81f41efa5b864849b6c1239634333f8cf1a1569
zevdvlpr <zevdvlpr@gmail.com>
2021-02-26 18:17:28 -0300

    🐛 fix: return to the login page

--- src/pages/logout.tsx
+++ src/pages/logout.tsx
@@ -1,9 +1,13 @@
+import { useEffect } from 'react';
+
 import { useRouter } from 'next/router';
 
 export default function Logout() {
   const router = useRouter();
 
-  router.push('/login');
+  useEffect(() => {
+    router.push('/login');
+  }, [])
 
   return <></>;
 }
