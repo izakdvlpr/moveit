da733dfaf0a568d04de1c1d7eaef04b763dafc2d
zevdvlpr <zevdvlpr@gmail.com>
2021-02-26 12:22:52 -0300

    ⚙️ perf: remove challenges provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,7 @@
 import { AppProps } from 'next/app';
 
-import { ChallengesProvider } from '../contexts/ChallengesContext';
-
 import '../styles/global.css';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <ChallengesProvider>
-      <Component {...pageProps} />;
-    </ChallengesProvider>
-  );
+  return <Component {...pageProps} />;
 }
