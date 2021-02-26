cff1afe327e9402dbc37f71582defad6bd8c55cf
zevdvlpr <zevdvlpr@gmail.com>
2021-02-26 18:11:02 -0300

    💡 chore: import auth context

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,17 @@
 import { AppProps } from 'next/app';
 
+import { AuthProvider } from '../contexts/AuthContext';
+
+import { SideBar } from '../components/SideBar';
+
 import '../styles/global.css';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+export default function App({ Component, pageProps, router }: AppProps) {
+  return (
+    <AuthProvider>
+      {router.pathname !== '/login' && <SideBar />}
+      
+      <Component {...pageProps} />;
+    </AuthProvider>
+  );
 }
