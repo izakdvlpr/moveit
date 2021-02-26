9c6a4fbe635c02597804a2829d778cabdae5fb5a
zevdvlpr <zevdvlpr@gmail.com>
2021-02-26 12:32:38 -0300

    🔨 refactor: change time

--- src/contexts/CountdownContext.tsx
+++ src/contexts/CountdownContext.tsx
@@ -28,7 +28,7 @@ export const CountdownContext = createContext({} as CountdownContextData);
 export function CountdownProvider({ children }: CountdownProviderProps) {
   const { startNewChallenge } = useContext(ChallengesContext);
 
-  const [time, setTime] = useState(0.1 * 60);
+  const [time, setTime] = useState(25 * 60);
   const [isActive, setIsActive] = useState(false);
   const [hasFinished, setHasFinished] = useState(false);
 
@@ -42,7 +42,7 @@ export function CountdownProvider({ children }: CountdownProviderProps) {
   function resetCountdown() {
     clearTimeout(countdownTimeout);
     setIsActive(false);
-    setTime(0.1 * 60);
+    setTime(25 * 60);
     setHasFinished(false);
   }
 
